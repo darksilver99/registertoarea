9b9387339215139c3eb2039f920abc70db0659e5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-24 05:24:19 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -3,7 +3,9 @@ import '/components/visitor_form_view_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/actions/actions.dart' as action_blocks;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'home_page_model.dart';
@@ -25,6 +27,11 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => HomePageModel());
+
+    // On page load action.
+    SchedulerBinding.instance.addPostFrameCallback((_) async {
+      await action_blocks.initCustomer(context);
+    });
 
     WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
   }
