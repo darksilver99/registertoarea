bbdfa8a74ee1a213e8ab8d76f15faf00fd6e37a1
darksilver99 <darksilver99@hotmail.com>
2024-10-29 10:00:50 +0700

    a

--- lib/custom_toon/CustomCarmera.dart
+++ lib/custom_toon/CustomCarmera.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter_web_qrcode_scanner/flutter_web_qrcode_scanner.dart';
 
 class CustomCarmera extends StatefulWidget {
   const CustomCarmera({super.key});
@@ -11,70 +12,28 @@ class CustomCarmera extends StatefulWidget {
   State<CustomCarmera> createState() => _CustomCarmeraState();
 }
 
-class _CustomCarmeraState extends State<CustomCarmera>{
+class _CustomCarmeraState extends State<CustomCarmera> {
+  final CameraController _controller = CameraController(autoPlay: true);
 
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
   @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
+  void dispose() {
+
+    _controller.stopVideoStream();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(child: _buildQrView(context))
-      ],
+    return Scaffold(
+      body: FlutterWebQrcodeScanner(
+        controller: _controller,
+        cameraDirection: CameraDirection.back,
+        stopOnFirstResult: true,
+        //set false if you don't want to stop video preview on getting first result
+        onGetResult: (result) {
+          Navigator.pop(context, result);
+        },
+      ),
     );
   }
-
-  Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 || MediaQuery.of(context).size.height < 400) ? 150.0 : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return Builder(builder: (BuildContextcontext) {
-      return QRView(
-        key: qrKey,
-        onQRViewCreated: _onQRViewCreated,
-        overlay: QrScannerOverlayShape(borderColor: Colors.red, borderRadius: 10, borderLength: 30, borderWidth: 10, cutOutSize: scanArea),
-        onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-      );
-    });
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      result = scanData;
-      controller.dispose();
-      Navigator.pop(context, result!.code ?? '');
-    });
-  }
-
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
 }
