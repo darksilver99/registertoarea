ff9db5c9b4e10d0104fb5745fece2067b4764f1e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-29 03:38:18 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/scan_bar_code_with_web.dart
+++ lib/custom_code/actions/scan_bar_code_with_web.dart
@@ -10,12 +10,12 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
-import 'package:register_to_area/custom_toon/CustomCarmera.dart';
+import 'package:register_to_area/custom_toon/CustomQrCodeScan.dart';
 
 Future<String> scanBarCodeWithWeb(BuildContext context) async {
   // Add your function code here!
   return await Navigator.push(
     context,
-    MaterialPageRoute(builder: (context) => const CustomCarmera()),
+    MaterialPageRoute(builder: (context) => const CustomQrCodeScan()),
   );
 }
